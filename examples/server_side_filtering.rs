cd24cade2d1e1da071b91a573ebf78a490b3bab7
YoEight <yo.eight@gmail.com>
2021-06-02 15:03:07 +0200

    Fix linting warnings.

--- examples/server_side_filtering.rs
+++ examples/server_side_filtering.rs
@@ -1,3 +1,8 @@
+#![allow(unused_attributes)]
+#![allow(unused_imports)]
+#![allow(unused_results)]
+#![allow(unused_variables)]
+
 use eventstore::{
     Client, Credentials, EventData, ExpectedRevision, ReadResult, SubEvent, SubscribeToAllOptions,
     SubscriptionFilter,
