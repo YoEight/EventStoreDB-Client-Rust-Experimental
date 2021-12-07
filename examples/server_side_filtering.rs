d346bc705eedf00564c58dbcdcfb6d095cfd3c0b
YoEight <yo.eight@gmail.com>
2021-12-07 17:54:49 +0100

    Implement better stream API.

--- examples/server_side_filtering.rs
+++ examples/server_side_filtering.rs
@@ -2,9 +2,10 @@
 #![allow(unused_imports)]
 #![allow(unused_results)]
 #![allow(unused_variables)]
+#![allow(unreachable_code)]
 
 use eventstore::{
-    Client, Credentials, EventData, ExpectedRevision, ReadResult, SubEvent, SubscribeToAllOptions,
+    Client, Credentials, EventData, ExpectedRevision, SubscribeToAllOptions, SubscriptionEvent,
     SubscriptionFilter,
 };
 use futures::TryStreamExt;
@@ -25,15 +26,14 @@ pub async fn exclude_system_events(client: &Client) -> Result<()> {
     let filter = SubscriptionFilter::on_event_type().regex("/^[^\\$].*/");
     let options = SubscribeToAllOptions::default().filter(filter);
 
-    let mut sub = client.subscribe_to_all(&options).await?;
+    let mut sub = client.subscribe_to_all(&options).await;
 
-    while let Some(event) = sub.try_next().await? {
-        if let SubEvent::EventAppeared(event) = event {
-            let stream_id = event.get_original_stream_id();
-            let revision = event.get_original_event().revision;
+    loop {
+        let event = sub.next().await?;
+        let stream_id = event.get_original_stream_id();
+        let revision = event.get_original_event().revision;
 
-            println!("Received event {}@{}", revision, stream_id);
-        }
+        println!("Received event {}@{}", revision, stream_id);
     }
     // endregion exclude-system
 
@@ -45,16 +45,15 @@ pub async fn event_type_prefix(client: &Client) -> Result<()> {
     let filter = SubscriptionFilter::on_event_type().add_prefix("customer-");
     let options = SubscribeToAllOptions::default().filter(filter);
 
-    let mut sub = client.subscribe_to_all(&options).await?;
+    let mut sub = client.subscribe_to_all(&options).await;
     // endregion event-type-prefix
 
-    while let Some(event) = sub.try_next().await? {
-        if let SubEvent::EventAppeared(event) = event {
-            let stream_id = event.get_original_stream_id();
-            let revision = event.get_original_event().revision;
+    loop {
+        let event = sub.next().await?;
+        let stream_id = event.get_original_stream_id();
+        let revision = event.get_original_event().revision;
 
-            println!("Received event {}@{}", revision, stream_id);
-        }
+        println!("Received event {}@{}", revision, stream_id);
     }
 
     Ok(())
@@ -65,16 +64,15 @@ pub async fn event_type_regex(client: &Client) -> Result<()> {
     let filter = SubscriptionFilter::on_event_type().regex("^user|^company");
     let options = SubscribeToAllOptions::default().filter(filter);
 
-    let mut sub = client.subscribe_to_all(&options).await?;
+    let mut sub = client.subscribe_to_all(&options).await;
     // endregion event-type-regex
 
-    while let Some(event) = sub.try_next().await? {
-        if let SubEvent::EventAppeared(event) = event {
-            let stream_id = event.get_original_stream_id();
-            let revision = event.get_original_event().revision;
+    loop {
+        let event = sub.next().await?;
+        let stream_id = event.get_original_stream_id();
+        let revision = event.get_original_event().revision;
 
-            println!("Received event {}@{}", revision, stream_id);
-        }
+        println!("Received event {}@{}", revision, stream_id);
     }
 
     Ok(())
@@ -85,16 +83,15 @@ pub async fn stream_prefix(client: &Client) -> Result<()> {
     let filter = SubscriptionFilter::on_stream_name().add_prefix("user-");
     let options = SubscribeToAllOptions::default().filter(filter);
 
-    let mut sub = client.subscribe_to_all(&options).await?;
+    let mut sub = client.subscribe_to_all(&options).await;
     // endregion stream-prefix
 
-    while let Some(event) = sub.try_next().await? {
-        if let SubEvent::EventAppeared(event) = event {
-            let stream_id = event.get_original_stream_id();
-            let revision = event.get_original_event().revision;
+    loop {
+        let event = sub.next().await?;
+        let stream_id = event.get_original_stream_id();
+        let revision = event.get_original_event().revision;
 
-            println!("Received event {}@{}", revision, stream_id);
-        }
+        println!("Received event {}@{}", revision, stream_id);
     }
 
     Ok(())
@@ -105,16 +102,15 @@ pub async fn stream_regex(client: &Client) -> Result<()> {
     let filter = SubscriptionFilter::on_event_type().regex("/^[^\\$].*/");
     let options = SubscribeToAllOptions::default().filter(filter);
 
-    let mut sub = client.subscribe_to_all(&options).await?;
+    let mut sub = client.subscribe_to_all(&options).await;
     // endregion stream-regex
 
-    while let Some(event) = sub.try_next().await? {
-        if let SubEvent::EventAppeared(event) = event {
-            let stream_id = event.get_original_stream_id();
-            let revision = event.get_original_event().revision;
+    loop {
+        let event = sub.next().await?;
+        let stream_id = event.get_original_stream_id();
+        let revision = event.get_original_event().revision;
 
-            println!("Received event {}@{}", revision, stream_id);
-        }
+        println!("Received event {}@{}", revision, stream_id);
     }
 
     Ok(())
@@ -125,20 +121,21 @@ pub async fn checkpoint_callback_with_interval(client: &Client) -> Result<()> {
     let filter = SubscriptionFilter::on_event_type().regex("/^[^\\$].*/");
     let options = SubscribeToAllOptions::default().filter(filter);
 
-    let mut sub = client.subscribe_to_all(&options).await?;
+    let mut sub = client.subscribe_to_all(&options).await;
     // endregion checkpoint-with-interval
 
     // region checkpoint
-    while let Some(event) = sub.try_next().await? {
+    loop {
+        let event = sub.next_subscription_event().await?;
         match event {
-            SubEvent::EventAppeared(event) => {
+            SubscriptionEvent::EventAppeared(event) => {
                 let stream_id = event.get_original_stream_id();
                 let revision = event.get_original_event().revision;
 
                 println!("Received event {}@{}", revision, stream_id);
             }
 
-            SubEvent::Checkpoint(position) => {
+            SubscriptionEvent::Checkpoint(position) => {
                 println!("checkpoint taken at {}", position.prepare);
             }
 
